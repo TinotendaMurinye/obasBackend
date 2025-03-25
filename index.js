6de923f23fe94b0c6f7f16624085e2b32b8da119
Tinotenda Murinye <murinyetinotenda@gmail.com git config --global user.name>
2025-03-25 13:12:39 +0200

    cmt

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const attendanceRouter = require("./routes/attendance"); // Adjust the path as n
 const usersRouter = require("./routes/users");
 require("dotenv").config();
 const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -22,7 +24,13 @@ app.get("/", (req, res) => {
   res.send("Wrong Location Move Along---This is My Space%");
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// SSL Certificates
+const sslOptions = {
+  key: fs.readFileSync('/etc/letsencrypt/live/srv690692.hstgr.cloud/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/srv690692.hstgr.cloud/fullchain.pem'),// Replace with the path to your SSL certificate
+};
+
+// Start the server with HTTPS
+https.createServer(sslOptions, app).listen(PORT, () => {
+  console.log(`Server is running on https://localhost:${PORT}`);
 });
